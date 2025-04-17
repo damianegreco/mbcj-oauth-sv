10a953ba36543a68a74d3348db1dfa5b73b73c5e
Damian Greco <damianegreco@gmail.com>
2025-04-17 15:05:25 -0300

    correcciones, mejoras de obtener clave

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const oauth = require('./oauth');
+const oauthRouter = require('./oauth');
 const middleware = require('./middleware');
 const obtenerClavePublica = require('./obtenerClave');
 
-module.exports = {oauth, middleware, obtenerClavePublica}
+module.exports = {oauthRouter, middleware, obtenerClavePublica}
--- obtenerClave.js
+++ obtenerClave.js
@@ -2,12 +2,19 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-const obtenerClavePublica = function(URL_oauth, ruta_directorio, nombre_archivo){
+function checkOrCreateDirectorio(ruta_directorio){
+  if (fs.existsSync(ruta_directorio)) return;
+  fs.mkdirSync(ruta_directorio, { recursive: true });
+  return;
+}
+
+function obtenerClavePublica(URL_oauth, ruta_directorio, nombre_archivo){
   const clavePath = path.join(ruta_directorio, nombre_archivo);
 
   axios.get(URL_oauth)
   .then((res) => {
     const clave_publica = res.data;
+    checkOrCreateDirectorio(ruta_directorio);
     fs.writeFileSync(clavePath, clave_publica, {encoding:'utf8'})
   })
   .catch((error) => {
